70741f3fa4dc3a59999dca373e0fe72feb448bdf
wkurylo88 <wkurylo88@gmail.com>
2018-01-10 14:13:05 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.sunshine.sync;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+
+import com.example.android.sunshine.data.WeatherContract;
+
+
+public class SunshineSyncUtils {
+
+    //  COMPLETED (1) Declare a private static boolean field called sInitialized
+    private static boolean sInitialized = false;
+
+    //  COMPLETED (2) Create a synchronized public static void method called initialize
+    synchronized public static void initialize(@NonNull final Context context) {
+        if (sInitialized) return;
+        sInitialized = true;
+
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+
+                ContentResolver resolver = context.getContentResolver();
+                // To save memory. We are askiong only for one Column to check. This column is "not null"
+                // If Table is not-empty, there shall be data.
+                String[] projection = new String[]{WeatherContract.WeatherEntry._ID};
+
+                Cursor checkSqlCursor = resolver.query(
+                        WeatherContract.WeatherEntry.CONTENT_URI,
+                        projection,
+                        null,
+                        null,
+                        null);
+
+                if (checkSqlCursor.getCount() == 0 || checkSqlCursor == null) {
+                    startImmediateSync(context);
+                }
+                /* Make sure to close the Cursor to avoid memory leaks! */
+                checkSqlCursor.close();
+                return null;
+            }
+        }.execute();
+    }
+    //  COMPLETED (3) Only execute this method body if sInitialized is false
+    //  COMPLETED (4) If the method body is executed, set sInitialized to true
+    //  COMPLETED (5) Check to see if our weather ContentProvider is empty
+    //  COMPLETED (6) If it is empty or we have a null Cursor, sync the weather now!
+
+    /**
+     * Helper method to perform a sync immediately using an IntentService for asynchronous
+     * execution.
+     *
+     * @param context The Context used to start the IntentService for the sync.
+     */
+    public static void startImmediateSync(@NonNull final Context context) {
+        Intent intentToSyncImmediately = new Intent(context, SunshineSyncIntentService.class);
+        context.startService(intentToSyncImmediately);
+    }
+}
